b72fecaecbce218f3066cd5ef77be68234b24752
PrashantYadav2412 <prashant.25b01010322@abes.ac.in>
2026-02-25 14:08:01 +0530

    learn diffrent data type

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log("Hello World")
+document.write("Hello world")
+
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+
+num = false;
+console.log(num);
+console.log(typeof(num));
+
+var num ="Prashant";
+console.log(num);
+console.log(typeof(num));
+
+var num =null;
+console.log(num);
+console.log(typeof(num));
+
+var num =undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 =symbol(4);
+console.log(sym1);
+
+const sym2 =symbol(4);
+console.log(sym1);
+if(sym==sym2){
+    console.log("true");
+}
+
+else{
+    console.log("false")//because each symbol is unique
+    
+}
+
+
