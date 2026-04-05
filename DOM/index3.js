66d1dfea919a4ca659a8a442a0d097bd00536a32
PrashantYadav2412 <prashant.25b01010322@abes.ac.in>
2026-04-05 14:55:24 +0530

    practice

---
+++ DOM/index3.js
@@ -0,0 +1,4 @@
+const x= document.getElementById("p1");
+
+// console.log(x);
+x.innerHTML= "PrashantYadav";
---
+++ Practice/condition.js
@@ -0,0 +1,71 @@
+//Trafic light
+
+// // let color = "red";
+// if(color === "red"){
+//     console.log("stop");
+// }
+// if(color==="yellow"){
+//     console.log("waiting");
+
+// }
+// if(color==="green"){
+//     console.log("go");
+// }
+
+//String condition
+
+// let str = "apple";
+// if(str[0]==="a" && str.length > 3){
+//     console.log("String is good");
+
+// }
+
+//popcorn price
+
+//let size="L";
+// if (size==="XL"){
+//     console.log("Price is:", 250);
+// }
+// if (size==="L"){
+//     console.log("Price is:", 200);
+// }
+// if (size==="M"){
+//     console.log("Price is:", 100);
+// }
+// if (size==="S"){
+//     console.log("Price is:", 50);
+// }
+
+//switch case
+
+let variable =parseInt( prompt("Enter a number between 1 and 7 to know the day of the week:"));
+switch(variable){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("tuesday")
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid input");
+}
+
+// let firstName = prompt("enter your first name");
+// let lastName = prompt("enter your last name");
+// let age = prompt("enter your age");
+// console.log(firstName,lastName,"is",age,"year old .");
